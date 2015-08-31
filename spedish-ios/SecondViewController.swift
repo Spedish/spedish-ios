18d53551335727aa61ba8540d4f8bb8446c70ae7
Huaxi Wang <huaxi.wang@gmail.com>
2015-08-31 01:12:15 -0700

    Created xcode project.

---
+++ spedish-ios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  spedish-ios
+//
+//  Created by Huaxi Wang on 8/31/15.
+//  Copyright (c) 2015 Huaxi Wang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
