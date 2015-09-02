f6768a202a3b378d4359b6b530462c734b04af49
Huaxi Wang <huaxi.wang@gmail.com>
2015-09-01 22:05:08 -0700

    Initial setup for ios

---
+++ spedish-ios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  spedish-ios
+//
+//  Created by Huaxi Wang on 9/1/15.
+//  Copyright (c) 2015 Spedish.com. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
